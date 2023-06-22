a900b7b271979478d75ad0a98a6760df8414818c
iamkinglana <ngetichenock32@gmail.com>
2023-06-22 22:54:58 +0300

    Commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+  def index
+    render json:Item.all ,status: :ok
+
+  end
+
+  def def show
+    items = Item.find(params[:id])
+    render json: items, include: :reviews
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all.order(rating: :desc)
+    render json: users, include: :items
+  end
 end
